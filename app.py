679f69f122f0673d68a9f38648435d9cdc36f207
Visa Lehtimäki <642229+ramla@users.noreply.github.com>
2025-10-29 06:29:33 +0200

    simple registration implemented

--- app.py
+++ app.py
@@ -1,20 +1,39 @@
+import sqlite3, db
 from flask import Flask
-from flask import render_template
-import sqlite3
+from flask import redirect, render_template, request
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    db = sqlite3.connect("database.db")
     db.execute("INSERT INTO visits (visited_at) VALUES (datetime('now'))")
-    db.commit()
-    result = db.execute("SELECT COUNT(*) FROM visits").fetchone()
-    count = result[0]
-    db.close()
-    paragraph = "Sivua on ladattu " + str(count) + " kertaa"
+    result = db.query("SELECT COUNT(*) FROM visits")
+    paragraph = "Sivua on ladattu " + str(result[0][0]) + " kertaa"
+    print(result)
     return render_template("index.html", second_p=paragraph)
 
 @app.route("/page/<int:page_id>")
 def page(page_id):
     return "Tämä on sivu " + str(page_id)
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (name, auth) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"
