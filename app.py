9bd71d33fdf27ae0b81930db74cb5ed24d9267f2
Visa Lehtimäki <642229+ramla@users.noreply.github.com>
2025-10-29 07:32:50 +0200

    implemented simple logon & session

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-import sqlite3, db
+import sqlite3, db, config
 from flask import Flask
-from flask import redirect, render_template, request
-from werkzeug.security import generate_password_hash
+from flask import redirect, render_template, request, session
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
     db.execute("INSERT INTO visits (visited_at) VALUES (datetime('now'))")
     result = db.query("SELECT COUNT(*) FROM visits")
-    paragraph = "Sivua on ladattu " + str(result[0][0]) + " kertaa"
+    paragraph = "Page loaded " + str(result[0][0]) + " times"
     print(result)
-    return render_template("index.html", second_p=paragraph)
+    return render_template("index.html", visits=paragraph)
 
 @app.route("/page/<int:page_id>")
 def page(page_id):
@@ -24,16 +25,37 @@ def register():
 @app.route("/create", methods=["POST"])
 def create():
     username = request.form["username"]
-    password1 = request.form["password1"]
+    password = request.form["password1"]
     password2 = request.form["password2"]
-    if password1 != password2:
-        return "VIRHE: salasanat eivät ole samat"
-    password_hash = generate_password_hash(password1)
+    if password != password2:
+        return "Passwords do not match"
+    password_hash = generate_password_hash(password)
 
     try:
         sql = "INSERT INTO users (name, auth) VALUES (?, ?)"
         db.execute(sql, [username, password_hash])
     except sqlite3.IntegrityError:
-        return "VIRHE: tunnus on jo varattu"
+        return "Username already taken"
 
-    return "Tunnus luotu"
+    # is this bm?
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    
+    sql = "SELECT auth FROM users WHERE name = ?"
+    password_hash = db.query(sql, [username])[0][0]
+
+    if check_password_hash(password_hash, password):
+        session["username"] = username
+        return redirect("/")
+    else:
+        return "Wrong username or password"
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
